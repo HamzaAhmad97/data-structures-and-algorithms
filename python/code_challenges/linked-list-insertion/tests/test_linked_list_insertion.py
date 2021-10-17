5a6ae099d30125d49e13064ba8cc1aea594fd771
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-10-17 21:57:51 +0300

    test: added the test for inserting a node after a node in the middle

--- python/code_challenges/linked-list-insertion/tests/test_linked_list_insertion.py
+++ python/code_challenges/linked-list-insertion/tests/test_linked_list_insertion.py
@@ -42,3 +42,14 @@ def test_insert_node_before_first_node():
     ll.insert_before('a','x')
     actual = ll.to_string()
     assert actual == expected
+
+@pytest.mark.skip('to do')
+def test_insert_after_a_node_in_the_middle():
+    expected = "{b} -> {c} -> {x} -> {a} -> Null"
+    ll = LinkedList()
+    ll.insert('a')
+    ll.insert('c')
+    ll.insert('b')
+    ll.insert_after('c','x')
+    actual = ll.to_string()
+    assert actual == expected
