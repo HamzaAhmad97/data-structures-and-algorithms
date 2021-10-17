8a775ff82f48e77a1309449c2d628d336214c57d
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-10-17 22:27:44 +0300

    test: added the tests along with their docstrings

--- python/code_challenges/linked-list-insertion/tests/test_linked_list_insertion.py
+++ python/code_challenges/linked-list-insertion/tests/test_linked_list_insertion.py
@@ -7,6 +7,9 @@ def test_version():
 
 @pytest.mark.skip('to do')
 def test_append():
+    """
+    Test the append method of a linked list
+    """
     expected = "{a} -> {x} -> Null"
     ll = LinkedList()
     ll.insert("a")
@@ -16,6 +19,9 @@ def test_append():
 
 @pytest.mark.skip('to do')
 def test_append_multiple_nodes_to_linked_list():
+    """
+    Test appending more than one node to a linked list
+    """
     expected = "{a} -> {x} -> {y} -> Null"
     ll = LinkedList()
     ll.insert("a")
@@ -26,6 +32,9 @@ def test_append_multiple_nodes_to_linked_list():
 
 @pytest.mark.skip('to do')
 def test_insert_node_before_a_node_in_the_middle():
+    """
+    Test inserting a node before some node in the middle of a linked list
+    """
     expected = "{a} -> {x} -> {b} -> Null"
     ll = LinkedList()
     ll.insert("b")
@@ -36,6 +45,9 @@ def test_insert_node_before_a_node_in_the_middle():
 
 @pytest.mark.skip('to do')
 def test_insert_node_before_first_node():
+    """
+    Test inserting a node before the first node in a linked list
+    """
     expected = "{x} -> {a} -> Null"
     ll = LinkedList()
     ll.insert('a')
@@ -45,6 +57,9 @@ def test_insert_node_before_first_node():
 
 @pytest.mark.skip('to do')
 def test_insert_after_a_node_in_the_middle():
+    """
+    Test inserting a node after a node in the middle of a linked list
+    """
     expected = "{b} -> {c} -> {x} -> {a} -> Null"
     ll = LinkedList()
     ll.insert('a')
@@ -56,6 +71,9 @@ def test_insert_after_a_node_in_the_middle():
 
 @pytest.mark.skip('to do')
 def test_insert_after_the_last_node():
+    """
+    Test inserting a node after the last node in a linked list
+    """
     expected = "{a} -> {x} -> Null"
     ll = LinkedList()
     ll.insert('a')
