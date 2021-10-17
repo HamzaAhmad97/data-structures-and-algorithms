6ce39205862a5146e49a7109f6a799187f58f1f1
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-10-17 21:54:12 +0300

    test: added the test for the append method

--- python/code_challenges/linked-list-insertion/tests/test_linked_list_insertion.py
+++ python/code_challenges/linked-list-insertion/tests/test_linked_list_insertion.py
@@ -1,5 +1,15 @@
 from linked_list_insertion import __version__
-
+from linked_list_insertion.linked_list_insertions import LinkedList
+import pytest
 
 def test_version():
     assert __version__ == '0.1.0'
+
+@pytest.mark.skip('to do')
+def test_append():
+    expected = "{a} -> {x} -> Null"
+    ll = LinkedList()
+    ll.insert("a")
+    ll.append('x')
+    actual = ll.to_string()
+    assert actual == expected
