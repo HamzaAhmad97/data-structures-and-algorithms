fddf5a81af8b990489ae8a7cebd033afa79d863e
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-10-19 19:06:48 +0300

    test: added a test for checking if the method raises an exception if k is negative

--- python/code_challenges/linked-list-kth/tests/test_linked_list_kth.py
+++ python/code_challenges/linked-list-kth/tests/test_linked_list_kth.py
@@ -24,4 +24,10 @@ def test_kth_from_end_returns_correct_value_when_k_equals_length_of_list():
     actual = ll.kth_from_end(2) # since the list is zero indexed
     assert actual == expected
 
+def test_kth_from_end_raises_an_exception_if_k_is_negative():
+    ll = LinkedList()
+    ll.insert('a')
+    with pytest.raises(OutOfRangeException, match="Distance cannot be negative or exceeding the number of nodes in the list."):
+        ll.kth_from_end(-1)
+
 
