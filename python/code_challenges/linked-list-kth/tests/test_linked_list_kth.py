9c98415928f7cc6f0c586c902aa2a2879e8f2b70
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-10-19 19:05:14 +0300

    test: added a test to check if the method returns the correct value if k lies in the middle of the linked list

--- python/code_challenges/linked-list-kth/tests/test_linked_list_kth.py
+++ python/code_challenges/linked-list-kth/tests/test_linked_list_kth.py
@@ -1,5 +1,17 @@
 from linked_list_kth import __version__
-
+from linked_list_kth.linked_list_kth import LinkedList
+import pytest
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_kth_from_end_returns_correct_value_if_number_is_smaller_than_list_length():
+    expected = 'b'
+    ll = LinkedList()
+    ll.insert('a')
+    ll.insert('b')
+    ll.insert('c')
+    actual = ll.kth_from_end(1)
+    assert actual == expected
+
+
