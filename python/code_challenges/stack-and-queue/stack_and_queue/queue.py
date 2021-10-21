545825ae492275bd4d23a6fb3586c3cf2fcbdccd
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-10-21 14:16:42 +0300

    test: added a test for enqueue

--- python/code_challenges/stack-and-queue/stack_and_queue/queue.py
+++ python/code_challenges/stack-and-queue/stack_and_queue/queue.py
@@ -1,3 +1,9 @@
+from stack_and_queue.node import Node
+#from node import Node
+
+class EmptyQueueException(Exception):
+    pass
+
 class Queue():
     def __init__(self):
         pass
--- python/code_challenges/stack-and-queue/tests/test_queue.py
+++ python/code_challenges/stack-and-queue/tests/test_queue.py
@@ -1,10 +1,12 @@
-from stack_and_queue.queue import Queue
-from stack_and_queue.node import Node
 import pytest
+from stack_and_queue.queue import Queue, EmptyQueueException
+from stack_and_queue.node import Node
+
 
 @pytest.fixture
 def empty_queue():
     return Queue()
+
 
 @pytest.fixture
 def queue():
@@ -12,4 +14,13 @@ def queue():
     queue.enqueue(1)
     queue.enqueue(2)
     return queue
+
+
+def test_queue_enqueue_to_empty_queue(empty_queue):
+    expected = 0
+    empty_queue.enqueue(0)
+    actual = empty_queue.front.value
+    assert actual == expected
+
+
 
