c42248bf27dcfca98df47f5e762fba26290dace5
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-10-21 12:56:15 +0300

    test: added a test for pushing multiple values onto a stack

--- python/code_challenges/stack-and-queue/tests/test_stack.py
+++ python/code_challenges/stack-and-queue/tests/test_stack.py
@@ -6,16 +6,19 @@ import pytest
 def send_empty_stack():
     stack = Stack()
 @pytest.fixture
-def senf_stack():
+def send_stack():
     stack = Stack()
     stack.push(1)
     stack.push(2)
 
 def test_push_onto_stack_when_stack_is_empty(send_empty_stack):
     expected = 1
-    actual = send_empty_stack.front.value
+    actual = send_empty_stack.top.value
     assert actual == expected
 
-
+def test_push_onto_stack_multiple_values(send_stack):
+    expected = 2
+    actual = send_stack.top.value
+    assert actual == expected
 
 
