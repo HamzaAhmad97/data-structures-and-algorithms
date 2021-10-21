4d50606dabd8d42af23cd4840c7a4c1042f69b35
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-10-21 15:03:28 +0300

    doc: added docstrings for the queue class and its methods

--- python/code_challenges/stack-and-queue/stack_and_queue/queue.py
+++ python/code_challenges/stack-and-queue/stack_and_queue/queue.py
@@ -3,13 +3,31 @@ from stack_and_queue.node import Node
 #from node import Node
 
 class EmptyQueueException(Exception):
+    """
+    An exception representing a queue is empty.
+
+    Args:
+        Exception (class): The Exception class.
+    """
     pass
 
 class Queue():
+    """
+    A class representing a stack.
+    """
     def __init__(self):
+        """
+        The constructor method for the class Queue. Initialize front and rear.
+        """
         self.front = None
         self.rear = None
     def __str__(self):
+        """
+        Return a string representation for a queue.
+
+        Returns:
+            str: A string representation for a queue.
+        """
         representation = "| "
         if not self.front:
             representation += 'Empty queue.'
@@ -26,6 +44,12 @@ class Queue():
             return representation
 
     def enqueue(self, value):
+        """
+        Push/ enqueue nodes to the back of a queue.
+
+        Args:
+            value (any): A node's value
+        """
         node = Node(value)
         node.next = None
         if not self.front:
@@ -36,6 +60,15 @@ class Queue():
         self.rear = node
 
     def dequeue(self):
+        """
+        Remove/ dequeue a node from the front of a queue.
+
+        Raises:
+            EmptyQueueException: If queue is empty.
+
+        Returns:
+            any: The value of the node dequeued.
+        """
         if self.is_empty():
             raise EmptyQueueException("Queue is empty, can't dequeue.")
         temp = self.front
@@ -44,11 +77,26 @@ class Queue():
         return temp.value
 
     def peek(self):
+        """
+        Return the value of the front node.
+
+        Raises:
+            EmptyQueueException: If the queue is empty.
+
+        Returns:
+            any: The value of the front node.
+        """
         if self.is_empty():
             raise EmptyQueueException("Queue is empty, can't peek.")
         return self.front.value
 
     def is_empty(self):
+        """
+        Check if the queue is empty.
+
+        Returns:
+            bool: True if queue is empty, False otherwise.
+        """
         return not self.front
 
 if __name__ == "__main__":
