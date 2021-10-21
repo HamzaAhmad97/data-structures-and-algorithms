1eade6c99aefd074ca9fb2fc79904a7b900cc9a8
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-10-21 14:17:19 +0300

    test: added a test for enqueueing multiple nodes

--- python/code_challenges/stack-and-queue/tests/test_queue.py
+++ python/code_challenges/stack-and-queue/tests/test_queue.py
@@ -23,4 +23,12 @@ def test_queue_enqueue_to_empty_queue(empty_queue):
     assert actual == expected
 
 
+def test_queue_enqueue_with_multiple_nodes(empty_queue):
+    expected = 2
+    empty_queue.enqueue(1)
+    empty_queue.enqueue(2)
+    actual = empty_queue.front.value
+    assert actual == expected
+
+
 
