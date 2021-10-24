1566b473fb3ef8c66afcfaadd13c6ed9fa110f2a
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-10-24 20:53:09 +0300

    test: added a test to check if enqueueing to an empty pseudo queue works

--- python/code_challenges/stack-queue-pseudo/tests/test_stack_queue_pseudo.py
+++ python/code_challenges/stack-queue-pseudo/tests/test_stack_queue_pseudo.py
@@ -1,5 +1,19 @@
+import pytest
 from stack_queue_pseudo import __version__
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+@pytest.fixture
+def empty_pseudo_queue():
+    return PseudoQueue()
+
+def test_enqueue_one_node_to_empty_pseudo_queue(empty_pseudo_queue):
+    empty_pseudo_queue.push(1)
+    expected = 1
+    actual = empty_pseudo_queue.dequeue()
+    assert actual == expected
+
+
+
