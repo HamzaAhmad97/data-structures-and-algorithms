9b1a625626a657dcf93946bc9324ef99ef310ba5
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-10-24 20:56:25 +0300

    feat: added the PseudoQueue class

--- python/code_challenges/stack-queue-pseudo/stack_queue_pseudo/pseudo_queue.py
+++ python/code_challenges/stack-queue-pseudo/stack_queue_pseudo/pseudo_queue.py
@@ -1,4 +1,25 @@
 class EmptyPseudoQueueException(Exception):
     pass
 
+class PseudoQueue:
+    def __init__(self):
+        self.taker = Stack()
+        self.giver = Stack()
+    def enqueue(self, value):
+        self.taker.push(value)
 
+        self.giver.top = None
+        current = self.taker.top
+        while current:
+            self.giver.push(current.value)
+            current = current.next
+
+    def dequeue(self):
+        if self.giver.is_empty():
+            raise EmptyPseudoQueueException(
+                "Pseudo queue is empty, can't dequeue.")
+
+        return self.giver.pop()
+
+    def __str__(self):
+        return str(self.giver)
