b197f2120b464d053e749fefddd3c4f9ef64637b
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-10-25 20:33:04 +0300

    test: added a test to check enqueueing a dog object

--- python/code_challenges/stack-queue-animal-shelter/tests/test_stack_queue_animal_shelter.py
+++ python/code_challenges/stack-queue-animal-shelter/tests/test_stack_queue_animal_shelter.py
@@ -1,5 +1,23 @@
 from stack_queue_animal_shelter import __version__
-
+import pytest
 
 def test_version():
     assert __version__ == '0.1.0'
+
+@pytest.fixture
+def animal_shelter():
+    return AnimalShelter()
+
+@pytest.fixture
+def dog():
+    return Dog("Coco", 3)
+
+@pytest.fixture
+def cat():
+    return Cat("Meme", 2)
+
+def test_enqueue_one_a_dog(animal_shelter,dog):
+    expected = "Coco"
+    animal_shelter.enqueue(dog)
+    actual = animal_shelter.dequeue('dog').name
+    assert actual == expected
