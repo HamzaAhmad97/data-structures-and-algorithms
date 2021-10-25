26252ae41b8e1e1dd242b1f4e66e08484fc3aae5
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-10-25 20:42:48 +0300

    feat: added the cat class

--- python/code_challenges/stack-queue-animal-shelter/stack_queue_animal_shelter/animal_shelter.py
+++ python/code_challenges/stack-queue-animal-shelter/stack_queue_animal_shelter/animal_shelter.py
@@ -8,4 +8,13 @@ class Dog:
     def __str__(self):
         return f"Name: {self.name} | Age: {self.age} | Type: {Dog.TYPE} | Next: {self.next}"
 
+class Cat:
+    TYPE = 'Cat'
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        self.next = None
+
+    def __str__(self):
+        return f"Name: {self.name} | Age: {self.age} | Type: {Cat.TYPE} | Next: {self.next}"
 
