367de247aa3b1639e3002f936e314a8d3fccbd43
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-10-26 19:14:24 +0300

    test: added a parametrized test which checks for 11 cases for the validate_brackets function

--- python/code_challenges/stack-queue-brackets/tests/test_stack_queue_brackets.py
+++ python/code_challenges/stack-queue-brackets/tests/test_stack_queue_brackets.py
@@ -1,5 +1,24 @@
+import pytest
 from stack_queue_brackets import __version__
-
 
 def test_version():
     assert __version__ == '0.1.0'
+
+@pytest.mark.parametrize(
+    "sequence, is_valid",
+    [
+        ('{}', True),
+        ('{}(){}', True),
+        ('()[[Extra Characters]]', True),
+        ('(){}[[]]', True),
+        ('{}{Code}[Fellows](())', True),
+        ('[({}]', False),
+        ('(](', False),
+        ('{(})', False),
+        ('{', False),
+        (')', False),
+        ('[}', False),
+    ],
+)
+def test_validate_brackets(sequence, is_valid):
+    assert validate_brackets(sequence) == is_valid
