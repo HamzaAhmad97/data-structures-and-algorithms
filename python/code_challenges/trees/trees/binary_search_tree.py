67024b76973af5e290bc7fc8b5a52aac40853510
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-10-28 20:24:02 +0300

    feat: added the binary search tree

---
+++ python/code_challenges/trees/trees/binary_search_tree.py
@@ -0,0 +1,47 @@
+class BinarySearchTree(BinaryTree):
+
+    def __init__(self, *args):
+        for itm in args:
+            self.add(itm)
+
+    def add(self, value):
+        if not self.root:
+            self.root = Node(value)
+            return
+        breadth = Queue()
+        breadth.enqueue(self.root)
+        while breadth.peek():
+            if breadth.front.left and breadth.front.right:
+                breadth.enqueue(breadth.front.left)
+                breadth.enqueue(breadth.front.right)
+                breadth.dequeue()
+            elif not breadth.front.left:
+                breadth.front.left = Node(value)
+                return
+            elif not breadth.front.right:
+                breadth.front.right = Node(value)
+                return
+
+    def contains(self, value):
+        try:
+            for item in self.post_order(self.root):
+                if item == value:
+                    return True
+            return False
+        except AttributeError:
+            return False
+
+    def __str__(self):
+        if not self.root:
+            return 'Empty tree.'
+        q = Queue()
+        q.enqueue(self.root)
+        representation = ""
+        while not q.is_empty():
+            front = q.dequeue()
+            representation += f"[Value > {front.value} | Left > {front.left if front.left else '....None'} | Right > {front.right if front.right else '....None'}]\n"
+            if front.left:
+                q.enqueue(front.left)
+            if front.right:
+                q.enqueue(front.right)
+        return representation
