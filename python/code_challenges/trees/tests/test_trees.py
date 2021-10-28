a4d83e524f1e376a87069d233c90e564b62ed0a5
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-10-28 20:03:54 +0300

    test: added a test to check the instantiation of an empty tree

--- python/code_challenges/trees/tests/test_trees.py
+++ python/code_challenges/trees/tests/test_trees.py
@@ -3,3 +3,11 @@ from trees import __version__
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_creating_an_empty_tree_instance():
+    expected = None
+    bt = BinarySearchTree()
+    actual = bt.root
+    assert actual == expected
+
+
