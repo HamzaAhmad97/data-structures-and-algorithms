2a803780bddf392c3f033ad6c71a3cda4d3af13b
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-10-28 20:16:11 +0300

    test: added a test to check instantiating a tree with a single node

--- python/code_challenges/trees/tests/test_trees.py
+++ python/code_challenges/trees/tests/test_trees.py
@@ -4,10 +4,17 @@ from trees import __version__
 def test_version():
     assert __version__ == '0.1.0'
 
-def test_creating_an_empty_tree_instance():
+def test_instantiating_an_empty_tree_instance():
     expected = None
     bt = BinarySearchTree()
     actual = bt.root
     assert actual == expected
+
+def test_instantiating_a_tree_with_a_single_root_node():
+    expected = "a"
+    bt = BinarySearchTree("a")
+    actual = bt.root.value
+    assert actual == expected
+
 
 
