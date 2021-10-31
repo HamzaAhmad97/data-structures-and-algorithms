2364229ad846498a2a911cda1c423b577d1f9388
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-10-31 14:30:26 +0200

    test: added a parametrized test to check the method max_value() which has 6 tests

--- python/code_challenges/tree-max/tests/test_tree_max.py
+++ python/code_challenges/tree-max/tests/test_tree_max.py
@@ -1,5 +1,22 @@
+import pytest
 from tree_max import __version__
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+@pytest.mark.parametrize(
+    "values, max",
+    [
+        ([1,2,3,4,5,6], 6),
+        ([0,0,4,8,6,-1,20], 20),
+        ([5,1,1,1,1],5),
+        ([-5,-2,-1,0,0],0),
+        ([0,1,0,1,0,1],1),
+        ([2],2),
+    ],
+)
+def test_max_value(values, max):
+    bt = BinarySearchTree(*values)
+    assert bt.max_value() == max
