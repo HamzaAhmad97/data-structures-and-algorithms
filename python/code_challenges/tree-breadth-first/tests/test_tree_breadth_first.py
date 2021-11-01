c4aafef43a1bc53c2537be9af17e0aceca654b87
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-11-01 16:55:26 +0200

    test: added a parametrized test to check for multiple cases, the input is the values that will get added to the tree

--- python/code_challenges/tree-breadth-first/tests/test_tree_breadth_first.py
+++ python/code_challenges/tree-breadth-first/tests/test_tree_breadth_first.py
@@ -3,3 +3,18 @@ from tree_breadth_first import __version__
 
 def test_version():
     assert __version__ == '0.1.0'
+
+@pytest.mark.parametrize(
+    "values, result",
+    [
+        ([1,2,3,4,5,6], [1,2,3,4,5,6]),
+        ([1,1,4,8,6,-1,20], [1,1,4,8,6,-1,20]),
+        ([5,1,1,1,1],[5,1,1,1,1]),
+        ([-5,-2,-1,0,0],[-5,-2,-1,0,0]),
+        ([2,2,1,1,2,2],[2,2,1,1,2,2]),
+        ([2],[2]),
+    ],
+)
+def test_breadth_first(values, result):
+    bt = BinarySearchTree(*values)
+    assert breadth_first(bt) == result
