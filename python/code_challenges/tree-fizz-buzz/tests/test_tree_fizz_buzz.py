df3896c5de30cf26edef81f3c911c6cc8a57dd39
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-11-02 19:55:54 +0200

    test: added a test to check fizz_buzz_tree with k equals 1

--- python/code_challenges/tree-fizz-buzz/tests/test_tree_fizz_buzz.py
+++ python/code_challenges/tree-fizz-buzz/tests/test_tree_fizz_buzz.py
@@ -3,3 +3,10 @@ from tree_fizz_buzz import __version__
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_fizz_buzz_tree_k_1():
+    expected = "1"
+    kt = KTree(1,1)
+    actual = fizz_buzz_tree(kt).front.value
+    assert actual == expected
+
