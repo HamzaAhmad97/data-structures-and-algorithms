1dc2655cb4ab59a6b7da5cd02444286d11ed7531
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-11-18 17:00:19 +0200

    feat: added LinkedList class

--- python/code_challenges/hashtable/hashtable/hashtable.py
+++ python/code_challenges/hashtable/hashtable/hashtable.py
@@ -5,6 +5,21 @@ class Node:
     def __str__(self):
         return f"{self.value}"
 
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    def insert(self,value):
+        self.head = Node(value, self.head)
+    def __str__(self):
+        current = self.head
+        representation = ""
+        while current != None:
+            representation += f"{{{current}}} -> "
+            if current._next == None:
+                representation += f"Null"
+            current = current._next
+        return representation
+
 
 
 if __name__ == "__main__":
