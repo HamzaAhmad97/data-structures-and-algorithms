0e0a91d03d4eefc13055987ff58b5223a9113ffd
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-11-18 17:00:53 +0200

    feat: added Hashtable class

--- python/code_challenges/hashtable/hashtable/hashtable.py
+++ python/code_challenges/hashtable/hashtable/hashtable.py
@@ -20,7 +20,48 @@ class LinkedList:
             current = current._next
         return representation
 
+class Hashtable:
+    def __init__(self, size = 1024):
+        self.__size = size
+        self.__buckets = [None for _ in range(size)]
+    def add(self):
+        pass
+    def __hash(self, key):
+        return sum([ord(char) for char in key]) * 7 % self.__size
 
+    def add(self, key, value):
+        index = self.__hash(key)
+
+        if not self.__buckets[index]:
+            self.__buckets[index] = LinkedList()
+        val = [key, value]
+        self.__buckets[index].insert(val)
+
+    def get(self, key):
+        index = self.__hash(key)
+        value = self.__buckets[index]
+        if value:
+            if isinstance(value, 'LinkedList'):
+                current = value.head
+                while current:
+                    if current.value[0] == key:
+                        return current.value[1]
+                    current = current._next
+            return value
+        return None
+                
+    def contains(self, key):
+        index = self.__hash(key)
+        value = self.__buckets[index]
+        if value:
+            if isinstance(value, 'LinkedList'):
+                current = value.head
+                while current:
+                    if current.value[0] == key:
+                        return True
+                    current = current._next
+            return True
+        return False
 
 if __name__ == "__main__":
     ht = Hashtable()
