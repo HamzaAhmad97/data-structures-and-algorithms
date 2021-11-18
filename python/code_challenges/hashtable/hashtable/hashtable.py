903fce80fa08f19d834397e8b3e82c4a5fc570e0
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-11-18 18:03:09 +0200

    test: added docstrings

--- python/code_challenges/hashtable/hashtable/hashtable.py
+++ python/code_challenges/hashtable/hashtable/hashtable.py
@@ -1,16 +1,53 @@
+"""
+The implementation of Node class, Linked list class, and Hashtable class. 
+"""
 class Node:
+    """
+    A data structure that stores a value, and references the next nodes.
+    """
     def __init__(self, value, _next = None):
+        """
+        The constructor method for Node class, takes a value and a reference to the next node. Initializes value, and _next.
+
+        Args:
+            value (list): A list containing a key/value pair.
+            _next (Node, optional): The next node to the current node. Defaults to None.
+        """
         self.value = value
         self._next = _next
     def __str__(self):
+        """
+        Generate a string representation of a node instance.
+
+        Returns:
+            str: a representation of a node instance as a string.
+        """
         return f"{self.value}"
 
 class LinkedList:
+    """
+    A data structure that consists of nodes that reference each other.
+    """
     def __init__(self):
+        """
+        The constructor method of a LinkedList instance which initializes the head property to None.
+        """
         self.head = None
     def insert(self,value):
+        """
+        Insert a node given a value to be stored in the new node to be inserted.
+
+        Args:
+            value (any): The value to be stored in a node.
+        """
         self.head = Node(value, self.head)
     def __str__(self):
+        """
+        Generate a string representation of a linked list instance.
+
+        Returns:
+            str: a string representation of a linked list instance.
+        """
         current = self.head
         representation = ""
         while current != None:
@@ -21,14 +58,39 @@ class LinkedList:
         return representation
 
 class Hashtable:
+    """
+    A data structure that is used to store key/value pairs effeciently.
+    """
     def __init__(self, size = 1024):
+        """
+        The constructor method of a Hashtable, takes size as an argument and initializes the __size property as well as the __buckets property to a list of size equals size.
+
+        Args:
+            size (int, optional): [description]. Defaults to 1024.
+        """
         self.__size = size
         self.__buckets = [None for _ in range(size)]
 
     def __hash(self, key):
+        """
+        Generate a hashcode correponding to the key provided.
+
+        Args:
+            key (str): a string that we want to generate a hashcode for.
+
+        Returns:
+            int: the hashcode of a string key.
+        """
         return sum([ord(char) for char in key]) * 7 % self.__size
 
     def add(self, key, value):
+        """
+        Add a key/value pair to a hashtable.
+
+        Args:
+            key (str): The key of a key/value pair.
+            value (any): The value corresponding to the key provided.
+        """
         index = self.__hash(key)
 
         if not self.__buckets[index]:
@@ -37,6 +99,15 @@ class Hashtable:
         self.__buckets[index].insert(val)
 
     def get(self, key):
+        """
+        Get the value corresponding to the key provided in the hashtable.
+
+        Args:
+            key (str): the key.
+
+        Returns:
+            any: the value stored corresponding to the key provided.
+        """
         index = self.__hash(key)
         value = self.__buckets[index]
         if value:
@@ -50,6 +121,15 @@ class Hashtable:
         return None
                 
     def contains(self, key):
+        """
+        Check if a key/value pair is exists in a hashtable.
+
+        Args:
+            key (str): a key.
+
+        Returns:
+            bool: True if the key exists in the hashtable, and False otherwise.
+        """
         index = self.__hash(key)
         value = self.__buckets[index]
         if value:
