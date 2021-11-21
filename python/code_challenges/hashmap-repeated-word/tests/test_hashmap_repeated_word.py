3ada8866f2a2eea45073ae6f30ff8b0a1f542c0e
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-11-21 21:38:42 +0200

    test: added a parametrized test to check if repeated_words raises an exception if number of words in provided string is insuffecient

--- python/code_challenges/hashmap-repeated-word/tests/test_hashmap_repeated_word.py
+++ python/code_challenges/hashmap-repeated-word/tests/test_hashmap_repeated_word.py
@@ -1,6 +1,6 @@
 import pytest
 from hashmap_repeated_word import __version__
-from hashmap_repeated_word.hashtable_repeated_word import repeated_word
+from hashmap_repeated_word.hashtable_repeated_word import repeated_word, InvalidNumberOfKeysError
 
 def test_version():
     assert __version__ == '0.1.0'
@@ -16,3 +16,15 @@ def test_version():
 )
 def test_repeated_word(text, repeated):
     assert repeated_word(text) == repeated
+
+@pytest.mark.parametrize(
+    "text",
+    [
+        "a",
+        "aa",
+        "",
+    ]
+)
+def test_repeated_word_raises_an_exception_on_invalid_number_of_words(text):
+    with pytest.raises(InvalidNumberOfKeysError):
+        repeated_word(text)
