626bcfda8737d5e1af78e504df51b19599cdb9e8
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-11-21 21:34:41 +0200

    test: added a parametrized test to check repeated word

--- python/code_challenges/hashmap-repeated-word/tests/test_hashmap_repeated_word.py
+++ python/code_challenges/hashmap-repeated-word/tests/test_hashmap_repeated_word.py
@@ -1,5 +1,18 @@
+import pytest
 from hashmap_repeated_word import __version__
-
+from hashmap_repeated_word.hashtable_repeated_word import repeated_word
 
 def test_version():
     assert __version__ == '0.1.0'
+
+@pytest.mark.parametrize(
+    "text,repeated",
+    [
+        ("a a", "a"),
+        ("a b c a", "a"),
+        ("a b b a", "b"),
+        ("A is a is A", "a")
+    ]
+)
+def test_repeated_word(text, repeated):
+    assert repeated_word(text) == repeated
