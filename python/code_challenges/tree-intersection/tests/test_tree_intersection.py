ecf21ca093f581648bb100e1dc1b9edd7732f663
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-11-22 14:49:40 +0200

    test: added a test to check if tree_intersection returns the correct list of values

--- python/code_challenges/tree-intersection/tests/test_tree_intersection.py
+++ python/code_challenges/tree-intersection/tests/test_tree_intersection.py
@@ -1,5 +1,21 @@
+import pytest
 from tree_intersection import __version__
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+@pytest.mark.parametrize(
+    "tree_a,tree_b,intersections",
+    [
+        ([1,2,3], [1,2,3], [1,2,3]),
+        (["1",2,"3"], [1,"2",3],None ),
+        (["1",1,2], ["1",1,2], ["1",1,2]),
+        ([1],[2],None),
+    ]
+)
+def test_tree_intersection(tree_a, tree_b, intersections):
+    a = BinarySearchTree(tree_a)
+    b = BinarySearchTree(tree_b)
+    assert tree_intersection(a,b) == intersections
+
