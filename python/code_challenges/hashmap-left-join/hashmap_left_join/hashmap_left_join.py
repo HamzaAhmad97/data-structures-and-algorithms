57236df9bf17f3a092c6cf1e811e7d80d34bbe6c
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-11-24 01:37:18 +0200

    test: rewrote test

--- python/code_challenges/hashmap-left-join/hashmap_left_join/hashmap_left_join.py
+++ python/code_challenges/hashmap-left-join/hashmap_left_join/hashmap_left_join.py
@@ -1,13 +1,20 @@
-from dependency_classes import Hashtable
+from hashmap_left_join.dependency_classes import Hashtable
+#from dependency_classes import Hashtable
 
 def left_join(ht1, ht2):
+
     container = []
     for itm in ht1._Hashtable__buckets:
         if itm:
             current = itm.head
             while current:
                 hashcode = ht1._Hashtable__hash(current.value[0])
-                cur = ht2._Hashtable__buckets[hashcode].head
+                cur = ht2._Hashtable__buckets[hashcode]
+                if not cur:
+                    container.append((current.value[0], (current.value[1], None)))
+                    current = current._next
+                    continue
+                cur = cur.head
                 while cur:
                     if cur.value[0] == current.value[0]:
 
@@ -20,14 +27,11 @@ def left_join(ht1, ht2):
 if __name__ == "__main__":
     ht1 = Hashtable(100)
     ht2 = Hashtable(100)
-    ht1.add("a", "11")
-    ht1.add("b", "22")
-    ht1.add("c", "33")
+    ht1.add("a", "a")
+    ht1.add("b", "b")
+    ht1.add("c", "c")
 
-    ht2.add("b", "2")
-    ht2.add("c", "3")
-    ht2.add("d", "4")
-    ht2.add("a", "4")
-    ht2.add("a", "dd")
-    ht2.add("a", "dddd")
-    left_join(ht1,ht2)
+    ht2.add("a", "1")
+    ht2.add("a", "2")
+    ht2.add("b", "1")
+    print(left_join(ht1,ht2))
--- python/code_challenges/hashmap-left-join/tests/test_hashmap_left_join.py
+++ python/code_challenges/hashmap-left-join/tests/test_hashmap_left_join.py
@@ -1,42 +1,102 @@
-from typing import Hashable
+
 import pytest
 from hashmap_left_join import __version__
 from hashmap_left_join.hashmap_left_join import left_join
+from hashmap_left_join.dependency_classes import Hashtable
 
 def test_version():
     assert __version__ == '0.1.0'
 
-@pytest.fixture
-def hashtable(vals):
-    ht = Hashable()
-    for key, val in vals:
-        ht.add(key, val)
-    return ht
 
-@pytest.mark.parametrize(
-    "left_hashtable,right_hashtable, expected", 
-    [
-        (
-            [("a", "a"), ("b", "b"),("c","c"),],
-            [("a","1"),("a","2"),("b", "1"),], 
-            [('a', ('a', '1')), ('a', ('a', '2')), ('b', ('b', '1')), ('c', ('c', None))]
-        ), 
-        (
-            [("a", "a"), ("b", "b"),("c","c"),],
-            [("a","1"),("a","2"),("a", "3"),], 
-            [('a', ('a', '1')), ('a', ('a', '2')), ('a', ('a', '3')), ('b', ('b', None)) ,('c', ('c', None))]
-        ), 
-        (
-            [("a", "a"), ("b", "b"),("c","c"),],
-            [("i","1"),("j","2"),("k", "1"),], 
-            [('a', ('a', None)), ('b', ('b', None)) ,('c', ('c', None))]
-        ), 
-        (
-            [],
-            [("i","1"),("j","2"),("k", "1"),], 
-            []
-        ), 
-    ]
-)
-def test_left_join(left_hashtable,right_hashtable, expected,hashtable):
-    assert left_join(hashtable(left_hashtable), hashtable(right_hashtable)) == expected
+def test_left_join_1():
+    expected = [('a', ('a', '2')), ('a', ('a', '1')), ('b', ('b', '1')), ('c', ('c', None))]
+    ht_left = Hashtable()
+    ht_right = Hashtable()
+    val_left = [("a", "a"), ("b", "b"),("c","c"),]
+    val_right = [("a","1"),("a","2"),("b", "1"),]
+    for key, value in val_left:
+        ht_left.add(key, value)
+    for key, value in val_right:
+        ht_right.add(key, value)
+    assert left_join(ht_left, ht_right) == expected
+
+def test_left_join_2():
+    expected = [('a', ('a', '3')), ('a', ('a', '2')), ('a', ('a', '1')), ('b', ('b', None)) ,('c', ('c', None))]
+    ht_left = Hashtable()
+    ht_right = Hashtable()
+    val_left = [("a", "a"), ("b", "b"),("c","c"),]
+    val_right = [("a","1"),("a","2"),("a", "3"),]
+    for key, value in val_left:
+        ht_left.add(key, value)
+    for key, value in val_right:
+        ht_right.add(key, value)
+    assert left_join(ht_left, ht_right) == expected
+
+def test_left_join_3():
+    expected = []
+    ht_left = Hashtable()
+    ht_right = Hashtable()
+    val_left = []
+    val_right = [("i","1"),("j","2"),("k", "1"),]
+    for key, value in val_left:
+        ht_left.add(key, value)
+    for key, value in val_right:
+        ht_right.add(key, value)
+    assert left_join(ht_left, ht_right) == expected
+
+def test_left_join_4():
+    expected = [('a', ('a', None)), ('b', ('b', None)) ,('c', ('c', None))]
+    ht_left = Hashtable()
+    ht_right = Hashtable()
+    val_left = [("a", "a"), ("b", "b"),("c","c"),]
+    val_right = [("i","1"),("j","2"),("k", "1"),]
+    for key, value in val_left:
+        ht_left.add(key, value)
+    for key, value in val_right:
+        ht_right.add(key, value)
+    assert left_join(ht_left, ht_right) == expected
+
+# @pytest.fixture(scope="function")
+# def hashtable_left(vals):
+#     ht = Hashtable()
+#     for key, val in vals:
+#         ht.add(key, val)
+#     return ht
+
+# @pytest.fixture(scope="function")
+# def hashtable_right(vals):
+#     ht = Hashtable()
+#     for key, val in vals:
+#         ht.add(key, val)
+#     return ht
+
+
+
+# @pytest.mark.parametrize(
+#     "hashtable_left,hashtable_right,expected", 
+#     [
+#         (
+#             [("a", "a"), ("b", "b"),("c","c"),],
+#             [("a","1"),("a","2"),("b", "1"),], 
+#             [('a', ('a', '1')), ('a', ('a', '2')), ('b', ('b', '1')), ('c', ('c', None))]
+#         ), 
+#         (
+#             [("a", "a"), ("b", "b"),("c","c"),],
+#             [("a","1"),("a","2"),("a", "3"),], 
+#             [('a', ('a', '1')), ('a', ('a', '2')), ('a', ('a', '3')), ('b', ('b', None)) ,('c', ('c', None))]
+#         ), 
+#         (
+#             [("a", "a"), ("b", "b"),("c","c"),],
+#             [("i","1"),("j","2"),("k", "1"),], 
+#             [('a', ('a', None)), ('b', ('b', None)) ,('c', ('c', None))]
+#         ), 
+#         (
+#             [],
+#             [("i","1"),("j","2"),("k", "1"),], 
+#             []
+#         ), 
+#     ],
+#     indirect=['hashtable_left','hashtable_right']
+# )
+# def test_left_join(hashtable_left, hashtable_right, expected):
+#     assert left_join(hashtable_left, hashtable_right) == expected
