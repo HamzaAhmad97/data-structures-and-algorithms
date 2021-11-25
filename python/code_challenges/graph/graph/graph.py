2df9199f6b684bb0f2444aaa54c3919b90c09134
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-11-25 18:22:18 +0200

    feat: added graph class

--- python/code_challenges/graph/graph/graph.py
+++ python/code_challenges/graph/graph/graph.py
@@ -54,3 +54,79 @@ class Stack:
         self.dq.pop()
 
 
+class Edge:
+    """ 
+    a class for Adding a new edge between two nodes in the graph
+    If specified, assigning a weight to the edge
+    Arguments: 2 nodes to be connected by the edge, weight (optional)
+    Returns: nothing
+    
+  """
+    def __init__(self, vertex, weight):
+        self.vertex = vertex
+        self.weight = weight
+
+
+class Graph:
+    def __init__(self):
+        """
+    Initalization for a hashmap to hold the vertices
+    """
+        self.__adjacency_list = {}
+
+    def add_node(self, value):
+        """
+      Method for Adding a node to the graph
+      Arguments: value
+      Returns: The added node
+    """
+        # new node
+        v = Vertex(value)
+        self.__adjacency_list[v] = []
+        return v
+
+    def size(self):
+        return len(self.__adjacency_list)
+
+    def add_edge(self, start_vertex, end_vertex, weight=0):
+        """Adds an edge to the graph"""
+        if start_vertex not in self.__adjacency_list:
+            raise KeyError("Start Vertex not found in graph")
+
+        if end_vertex not in self.__adjacency_list:
+            raise KeyError("End Vertex not found in graph")
+
+        edge = Edge(end_vertex, weight)
+        self.__adjacency_list[start_vertex].append(edge)
+
+    def get_nodes(self):
+        """
+    Method to get all nodes in Graph
+    Arguments: None
+    return: All nodes
+    """
+        return self.__adjacency_list.keys()
+
+    def get_neighbors(self, vertex):
+        """ """
+        return self.__adjacency_list.get(vertex, [])
+
+    def breadth_first_search(self, start_vertex, action=(lambda vertex: None)):
+        queue = Queue()
+        visited = set()
+
+        queue.enqueue(start_vertex)
+        visited.add(start_vertex)
+
+        while len(queue):
+            current_vertex = queue.dequeue()
+            action(current_vertex)
+
+            neighbors = self.get_neigbors(current_vertex)
+
+            for edge in neighbors:
+                neighbor = edge.vertex
+
+                if neighbour not in visited:
+                    visited.add(neighbor)
+                    queue.enqueue(neighbour)
