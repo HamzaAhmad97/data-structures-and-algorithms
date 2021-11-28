a5965cd0fa6b9cdb8e5b8ad89c0216b8dc0cac6e
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-11-28 18:45:19 +0200

    build: moved the code to the right file

--- python/code_challenges/graph/tests/test_graph.py
+++ python/code_challenges/graph/tests/test_graph.py
@@ -91,39 +91,3 @@ def test_get_neighbors():
 
     assert neighbor_edge.weight == 44
 
-def test_breadth_first_search_a():
-    expected = ['a', 'b', 'c', 'e', 'd']
-    gph = Graph()
-    a = gph.add_node("a")
-    b = gph.add_node("b")
-    c = gph.add_node("c")
-    d = gph.add_node("d")
-    e = gph.add_node("e")
-    # directed graph
-    gph.add_edge(a,b,weight=1)
-    gph.add_edge(a,c,weight=1)
-    gph.add_edge(c,d,weight=1)
-    gph.add_edge(a,e,weight=1)
-    gph.add_edge(b,e,weight=1)
-    assert gph.breadth_first_search(a) == expected
-
-def test_breadth_first_search_b():
-    expected = ['d', 'c', 'a', 'b', 'e']
-    gph = Graph()
-    a = gph.add_node("a")
-    b = gph.add_node("b")
-    c = gph.add_node("c")
-    d = gph.add_node("d")
-    e = gph.add_node("e")
-    gph.add_edge(a,b,weight=1)
-    gph.add_edge(a,c,weight=1)
-    gph.add_edge(c,d,weight=1)
-    gph.add_edge(a,e,weight=1)
-    gph.add_edge(b,e,weight=1)
-    # now it becomes an undirected graph
-    gph.add_edge(b,a,weight=1)
-    gph.add_edge(c,a,weight=1)
-    gph.add_edge(d,c,weight=1)
-    gph.add_edge(e,a,weight=1)
-    gph.add_edge(e,b,weight=1)
-    assert gph.breadth_first_search(d) == expected
